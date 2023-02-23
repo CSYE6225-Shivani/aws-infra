13d0c19a9da46d7465bf1b6fcfb9e0439a68bed1
Shivani31996 <shivani.chavan96@gmail.com>
2023-02-23 00:06:31 -0500

    formatting

--- network.tf
+++ network.tf
@@ -142,10 +142,10 @@ resource "aws_security_group" "application-sg" {
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
