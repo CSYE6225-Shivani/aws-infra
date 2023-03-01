16f8e560b278beaf8c1c80df548d979310b6598e
Shivani31996 <shivani.chavan96@gmail.com>
2023-03-01 15:54:54 -0500

    Added bucket dependency

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -43,6 +43,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_bucket_lifecycle_config" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access" {
+  depends_on = [aws_s3_bucket.s3_bucket]
   bucket = aws_s3_bucket.s3_bucket.id
 
   block_public_acls       = true
